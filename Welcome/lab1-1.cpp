27306e2f46bfd4654aaf820d5dfb88d9a4c72d68
Show Wai Yan <showwaiyan555@gmail.com>
2023-08-22 20:57:01 +0800

    Initializing the files

---
+++ Welcome/lab1-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "  CCCC      +         +   \n";
+    cout << " C          +         +   \n";
+    cout << "C        +++++++   +++++++\n";
+    cout << " C          +         +   \n";
+    cout << "  CCCC      +         +   "<<endl;
+}
