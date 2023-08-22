eb029f2398fa69ae49390970f7083adcca77d7a5
Show Wai Yan <showwaiyan555@gmail.com>
2023-08-22 22:33:05 +0800

    Labwork-1 problems finished

---
+++ LabWork1/lab1-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   cout << "A   A^2   A^3\n" ;
+   cout << "1    1     1\n" ;
+   cout << "2    4     8\n" ;
+   cout << "3    9     27\n" ;
+   cout << "4    16    64" << endl;
+}
