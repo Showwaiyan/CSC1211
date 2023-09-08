ba0cc9c20de7dc2302e445a82307727296700406
Show Wai Yan <showwaiyan@gmail.com>
2023-09-08 18:30:51 +0800

    LabWork2 assignments files finished

---
+++ LabWork2/lab2-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    cout << "a" << "   " << "b" << "   " << "Pow(a,b)\n";
+    cout << 1 << "   " << 2 << "   " << pow(1,2)<< "\n";
+    cout << 2 << "   " << 3 << "   " << pow(2,3)<< "\n";
+    cout << 3 << "   " << 4 << "   " << pow(3,4)<< "\n";
+    cout << 4 << "   " << 5 << "   " << pow(4,5)<< "\n";
+    cout << 5 << "   " << 6 << "   " << pow(5,6)<< endl;
+    return 0;
+}
